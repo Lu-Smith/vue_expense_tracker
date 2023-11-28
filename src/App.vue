db3166dc2a0e1d964efb9975ca1e6e288d2b4f5e
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-28 15:32:10 +0000

    Updated TransactionList

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,23 @@
   <Header />
   <div class="container">
     <Balance />
+    <IncomeExpenses />
+    <TransactionList />
   </div>
 </template>
 
 <script lang="ts">
  import Header from './components/Header.vue';
  import Balance from './components/Balance.vue';
+ import IncomeExpenses from './components/IncomeExpenses.vue';
+ import TransactionList from './components/TransactionList.vue';
 
  export default {
   components: {
     Header,
-    Balance
+    Balance,
+    IncomeExpenses,
+    TransactionList
   }
  }
 </script>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,11 @@
+<template>
+    <h3>History</h3>
+    <ul id="list" class="list">
+        <li>
+        cost <span>$0</span
+        ><button class="delete-btn">
+            x
+        </button>
+        </li>
+    </ul>
+</template>
