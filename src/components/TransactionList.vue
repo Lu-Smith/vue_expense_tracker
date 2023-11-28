63f68c2bc8af11f981c45b671ee14b0787b85583
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-28 15:36:45 +0000

    Updated TransactionList.vue

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,11 +1,13 @@
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
-        <li>
-        cost <span>$0</span
-        ><button class="delete-btn">
-            x
-        </button>
+        <li class="minus">
+        Cash <span>-$120</span>
+        <button class="delete-btn">x</button>
+        </li>
+        <li class="plus">
+        Income <span>$800</span>
+        <button class="delete-btn">x</button>
         </li>
     </ul>
 </template>
