53abcd09edd5f43be263dd827db034e0a52ef38f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-28 19:27:36 +0000

    Updated styling

--- src/components/IncomeExpenses.vue
+++ src/components/IncomeExpenses.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="flex items-center flex-row justify-center bg-slate-200 mt-2 mb-6">
         <div 
-        class="py-3 px-8 flex items-center flex-col justify-center w-30 border-solid border-r-2 border-sky-50">
+        class="py-3 px-8 flex items-center flex-col justify-center w-30 
+        border-solid border-r-2 border-sky-50 shadow-sm">
             <h4>Income</h4>
             <p id="money-plus" class="text-green-600">+$0</p>
         </div>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,13 +1,44 @@
 <template>
     <h3 class="font-semibold border-solid border-b-2 border-slate-300">History</h3>
     <ul id="list" class="list">
-        <li class="bg-slate-200 mt-2 px-2 py-1 flex items-center flex-row justify-between">
+      <li
+        class="relative bg-slate-200 mt-2 px-2 py-1 flex items-center 
+          flex-row justify-between shadow-sm border-solid border-r-4 border-red-600"
+        @mouseover="showButton = true"
+        @mouseleave="showButton = false"
+      >
         Cash <span>-$120</span>
-        <button class="delete-btn">x</button>
-        </li>
-        <li class="bg-slate-200 mt-2 px-2 py-1 flex items-center flex-row justify-between">
+        <button class="hidden absolute bg-red-600 px-2 text-white" v-show="showButton" @mouseover.stop> <!-- Use @mouseover.stop to prevent event propagation -->
+          x
+        </button>
+      </li>
+      <li
+        class="relative bg-slate-200 mt-2 px-2 py-1 flex items-center 
+          flex-row justify-between shadow-sm border-solid border-r-4 border-green-600"
+        @mouseover="showButton = true"
+        @mouseleave="showButton = false"
+      >
         Income <span>$800</span>
-        <button class="delete-btn">x</button>
-        </li>
+        <button class="hidden absolute bg-red-600 px-2 text-white" v-show="showButton" @mouseover.stop>
+          x
+        </button>
+      </li>
     </ul>
-</template>
+  </template>
+  
+  <script lang="ts">
+  export default {
+    data() {
+      return {
+        showButton: false,
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  li:hover .hidden {
+    display: flex;
+    margin-left: -35px;
+  }
+  </style>
