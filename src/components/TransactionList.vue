91f010f11e52ea02a32d2c83deb0a2dbf68ea75f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-28 19:04:54 +0000

    Updated styling TransactionList

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,11 +1,11 @@
 <template>
-    <h3>History</h3>
+    <h3 class="font-semibold border-solid border-b-2 border-slate-300">History</h3>
     <ul id="list" class="list">
-        <li class="minus">
+        <li class="bg-slate-200 mt-2 px-2 py-1 flex items-center flex-row justify-between">
         Cash <span>-$120</span>
         <button class="delete-btn">x</button>
         </li>
-        <li class="plus">
+        <li class="bg-slate-200 mt-2 px-2 py-1 flex items-center flex-row justify-between">
         Income <span>$800</span>
         <button class="delete-btn">x</button>
         </li>
