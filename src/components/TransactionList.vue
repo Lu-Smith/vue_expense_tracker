9d88da2ea1d64fb45c74e1a53160568c98bd5f14
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-28 19:50:29 +0000

    Updated TransactionList.tsx

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,27 +1,27 @@
 <template>
     <h3 class="font-semibold border-solid border-b-2 border-slate-300">History</h3>
-    <ul id="list" class="list">
+    <ul id="list" class="duration-300">
       <li
         class="relative bg-slate-200 mt-2 px-2 py-1 flex items-center 
-          flex-row justify-between shadow-sm border-solid border-r-4 border-red-600"
+          flex-row justify-between shadow-sm border-solid border-r-4 border-red-600 duration-300"
         @mouseover="showButton = true"
-        @mouseleave="showButton = false"
       >
-        Cash <span>-$120</span>
-        <button class="hidden absolute bg-red-600 px-2 text-white duration-300" v-show="showButton" @mouseover.stop> <!-- Use @mouseover.stop to prevent event propagation -->
+      <button class="hidden bg-red-600 px-2 text-white duration-300" v-show="showButton" @mouseover.stop> <!-- Use @mouseover.stop to prevent event propagation -->
           x
         </button>
+        Cash <span>-$120</span>
       </li>
       <li
         class="relative bg-slate-200 mt-2 px-2 py-1 flex items-center 
-          flex-row justify-between shadow-sm border-solid border-r-4 border-green-600"
+          flex-row justify-between shadow-sm border-solid border-r-4 border-green-600 duration-300"
         @mouseover="showButton = true"
-        @mouseleave="showButton = false"
+    
       >
-        Income <span>$800</span>
-        <button class="hidden absolute bg-red-600 px-2 text-white duration-300" v-show="showButton" @mouseover.stop>
+      <button class="hidden bg-red-600 px-2 text-white duration-300" v-show="showButton" @mouseover.stop>
           x
         </button>
+        Income <span>$800</span>
+   
       </li>
     </ul>
   </template>
@@ -39,6 +39,5 @@
   <style scoped>
   li:hover .hidden {
     display: flex;
-    margin-left: -35px;
   }
   </style>
