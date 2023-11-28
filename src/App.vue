4b75a15bb66e067653ee54a217eec3365c6cdcdd
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-28 13:41:37 +0000

    Updated basic files

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
+    <h1 class='text-green-800'>
+    Hello!
   </h1>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import './input.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
