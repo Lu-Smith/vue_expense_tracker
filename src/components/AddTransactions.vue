d1ba81faa6f80fb9f0d00a6a96fb4860041aca9e
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-29 14:41:54 +0000

    Updated logic in AddTransactios.vue

--- src/components/AddTransactions.vue
+++ src/components/AddTransactions.vue
@@ -8,13 +8,14 @@
             <label 
             for="text"
             class="font-semibold mt-2 text-base">
-                Income/Expense
+            Income/Expense
             </label>
             <input 
             type="text" 
             id="text" 
             placeholder="Enter text..."
-            class="px-2 py-1 w-full text-base"/>
+            class="px-2 py-1 w-full text-base"
+            v-model="text"/>
         </div>
         <div class="flex items-start justify-center flex-col gap-2 w-full">
             <label 
@@ -23,17 +24,22 @@
             Amount 
             </label>
             <input
-            type="text"
+            type="amount"
             id="amount"
             placeholder="Enter amount..."
-            class="px-2 py-1 w-full text-base"/>
+            class="px-2 py-1 w-full text-base"
+            v-model="amount"/>
         </div>
         <button class="bg-purple-400 hover:bg-purple-300 mt-4 px-2 py-1 w-full duration-300 shadow-sm hover:w-4/5 m-auto">Add transaction</button>
     </form>
 </template>
 
 <script lang="ts" setup>
-    const onSubmit = () => {
-        console.log('submit');
-    }
+import { ref } from 'vue';
+
+const text = ref('');
+const amount = ref(0);
+const onSubmit = () => {
+    console.log(text.value, amount.value);
+};
 </script>
