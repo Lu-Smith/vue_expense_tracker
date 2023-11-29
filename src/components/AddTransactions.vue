3e65b61c82b3f9dcfa7ad744e114f0e1c21310bf
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-29 14:29:12 +0000

    Updated logic in AddTransaction.vue

--- src/components/AddTransactions.vue
+++ src/components/AddTransactions.vue
@@ -1,6 +1,9 @@
 <template>
      <h3 class="font-semibold mt-6 border-solid border-b-2 border-slate-300">Add new transaction</h3>
-    <form id="form" class="flex items-start justify-center flex-col gap-2 w-full" >
+    <form 
+    id="form" 
+    class="flex items-start justify-center flex-col gap-2 w-full"
+    @submit.prevent="onSubmit" >
         <div class="flex items-start justify-center flex-col gap-2 w-full">
             <label 
             for="text"
@@ -28,3 +31,9 @@
         <button class="bg-purple-400 hover:bg-purple-300 mt-4 px-2 py-1 w-full duration-300 shadow-sm hover:w-4/5 m-auto">Add transaction</button>
     </form>
 </template>
+
+<script lang="ts" setup>
+    const onSubmit = () => {
+        console.log('submit');
+    }
+</script>
