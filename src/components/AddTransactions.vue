4c4db640ff879540979becd20d9fea5cb950b98c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-29 11:18:51 +0000

    Updated AddTransactions

--- src/components/AddTransactions.vue
+++ src/components/AddTransactions.vue
@@ -1,20 +1,20 @@
 <template>
-     <h3>Add new transaction</h3>
+     <h3 class="font-semibold mt-6 border-solid border-b-2 border-slate-300">Add new transaction</h3>
     <form id="form" >
         <div class="form-control">
-        <label for="text">Text</label>
-        <input type="text" id="text" placeholder="Enter text..."/>
+            <label for="text">Income/Expense</label>
+            <input type="text" id="text" placeholder="Enter text..."/>
         </div>
         <div class="form-control">
-        <label for="amount"
-            >Amount <br />
-            (negative - expense, positive - income)</label
-        >
-        <input
+            <label 
+            for="amount"
+            class="font-semibold">
+            Amount <br />(negative - expense, positive - income)
+            </label>
+            <input
             type="text"
             id="amount"
-            placeholder="Enter amount..."
-        />
+            placeholder="Enter amount..."/>
         </div>
         <button class="btn">Add transaction</button>
     </form>
