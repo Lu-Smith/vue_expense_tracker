53c280d1306c4e3b3275870c05d384db9e5d8d66
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-29 14:02:01 +0000

    Added total prop to Balance

--- src/App.vue
+++ src/App.vue
@@ -22,9 +22,9 @@
 
  const transactions = ref([
   {id: 1, text: 'Flowers', amount: -19.99},
-  {id: 2, text: 'Bread', amount: -3.40},
+  {id: 2, text: 'Bread', amount: -2.40},
   {id: 3, text: 'Milk', amount: -1.30},
-  {id: 4, text: 'Salary', amount: 199.99}]);
+  {id: 4, text: 'Salary', amount: 799.99}]);
 
   const total = computed(() => {
     return transactions.value.reduce((acc, transaction) => {
--- src/components/Balance.vue
+++ src/components/Balance.vue
@@ -1,4 +1,13 @@
 <template>
     <h4 class="tracking-wide mt-6">Your Balance</h4>
-    <h2 id="balance" class="font-semibold text-2xl">$0.00</h2>
+    <h2 id="balance" class="font-semibold text-2xl">${{ total }}</h2>
 </template>
+
+<script lang="ts" setup>
+    const props = defineProps({
+        total: {
+            type: Number,
+            required: true,
+        }
+    })
+</script>
