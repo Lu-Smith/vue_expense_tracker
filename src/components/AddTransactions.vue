0eccdf62448bf3be5f95b4de460a475e65621a00
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-29 11:26:06 +0000

    Updated styling in AddTransaction.vue

--- src/components/AddTransactions.vue
+++ src/components/AddTransactions.vue
@@ -1,21 +1,30 @@
 <template>
      <h3 class="font-semibold mt-6 border-solid border-b-2 border-slate-300">Add new transaction</h3>
-    <form id="form" >
-        <div class="form-control">
-            <label for="text">Income/Expense</label>
-            <input type="text" id="text" placeholder="Enter text..."/>
+    <form id="form" class="flex items-start justify-center flex-col gap-2 w-full" >
+        <div class="flex items-start justify-center flex-col gap-2 w-full">
+            <label 
+            for="text"
+            class="font-semibold mt-2">
+                Income/Expense
+            </label>
+            <input 
+            type="text" 
+            id="text" 
+            placeholder="Enter text..."
+            class="px-2 py-1 w-full"/>
         </div>
-        <div class="form-control">
+        <div class="flex items-start justify-center flex-col gap-2 w-full">
             <label 
             for="amount"
             class="font-semibold">
-            Amount <br />(negative - expense, positive - income)
+            Amount 
             </label>
             <input
             type="text"
             id="amount"
-            placeholder="Enter amount..."/>
+            placeholder="Enter amount..."
+            class="px-2 py-1 w-full"/>
         </div>
-        <button class="btn">Add transaction</button>
+        <button class="bg-purple-400 px-2 py-1">Add transaction</button>
     </form>
 </template>
