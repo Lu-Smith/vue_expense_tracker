35cc5b63ba351fa5a651b112e76ef0e133957cba
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-29 12:53:56 +0000

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,24 @@
     <div class="">
       <Balance />
       <IncomeExpenses />
-      <TransactionList />
+      <TransactionList :transactions="transactions"/>
       <AddTransactions />
     </div>
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
  import Header from './components/Header.vue';
  import Balance from './components/Balance.vue';
  import IncomeExpenses from './components/IncomeExpenses.vue';
  import TransactionList from './components/TransactionList.vue';
  import AddTransactions from './components/AddTransactions.vue';
 
- export default {
-  components: {
-    Header,
-    Balance,
-    IncomeExpenses,
-    TransactionList,
-    AddTransactions
-  }
- }
+ import { ref } from 'vue';
+
+ const transactions = ref([
+  {id: 1, text: 'Flowers', amount: -19.99},
+  {id: 2, text: 'Bread', amount: -3.40},
+  {id: 3, text: 'Milk', amount: -1.30},
+  {id: 4, text: 'Salary', amount: 199.99}]);
 </script>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -15,34 +15,13 @@
           </button>
            {{ transaction.text }} <span>${{ transaction.amount }}</span>
       </li>  
-      <!-- </li>
-      <li
-        class="relative bg-slate-200 mt-2 px-2 py-1 flex items-center text-base
-          flex-row justify-between shadow-sm border-solid border-r-4 border-green-600 duration-300"
-        @mouseover="showButton = true"
-    
-      > -->
-      <!-- <button class="hidden bg-red-600 px-2 text-white duration-300" v-show="showButton" @mouseover.stop>
-          x
-      </button> -->
-        <!-- Income <span>$800</span> -->
-      <!-- </li> -->
     </ul>
   </template>
   
-  <script lang="ts">
-  export default {
-    data() {
-      return {
-        showButton: false,
-        transactions: [
-        {id: 1, text: 'Flowers', amount: -19.99},
-        {id: 2, text: 'Bread', amount: -3.40},
-        {id: 3, text: 'Milk', amount: -1.30},
-        {id: 4, text: 'Salary', amount: 199.99},      ]
-      };
-    },
-  };
+  <script lang="ts" setup>
+  import { ref } from 'vue';
+  
+  const showButton = ref(false);
   </script>
   
   <style scoped>
